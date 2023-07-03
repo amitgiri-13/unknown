4272fe6d2ada0791e30cd038508d1409f93bca0a
Prabin Pyakurel <prabinpyakurel82@gmail.com>
2023-07-03 21:38:31 +0545

    made some changes

---
+++ test_sum.py
@@ -0,0 +1,15 @@
+import pytest
+from sum import solution
+s=solution()
+nums1=[2,7,11,14]
+nums2=[3,2,4]
+nums3=[3,3]
+
+def test_calc1():
+    assert s.calc(nums1,9)==(0,1)
+
+def test_calc2():
+    assert s.calc(nums2,6)==(1,2)
+
+def test_calc3():
+    assert s.calc(nums3,6)==(0,1)
