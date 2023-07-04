b81765aeda204e00995aab71d9d18f56b3c4052c
Prabin Pyakurel <prabinpyakurel82@gmail.com>
2023-07-04 18:27:36 +0545

    its working but still not best

--- test_sum.py
+++ test_sum.py
@@ -4,6 +4,7 @@ s=solution()
 nums1=[2,7,11,14]
 nums2=[3,2,4]
 nums3=[3,3]
+nums4=[3,2,3]
 
 def test_calc1():
     assert s.calc(nums1,9)==(0,1)
@@ -13,3 +14,6 @@ def test_calc2():
 
 def test_calc3():
     assert s.calc(nums3,6)==(0,1)
+
+def test_calc4():
+    assert s.calc(nums4,6)==(0,2)
